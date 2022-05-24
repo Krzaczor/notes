367a7c1d74b4fd789b6119a040cbd03dc380016f
Krzaczor <jbielecki18@gmail.com>
2022-05-24 22:19:03 +0200

    create actions for notes list

--- src/components/notesLayout/navNotes/NavNotes.tsx
+++ src/components/notesLayout/navNotes/NavNotes.tsx
@@ -3,9 +3,12 @@ import Actions from 'components/shared/actions/Actions';
 import Button from 'components/shared/button/Button';
 import MoreOptions from 'components/moreOptions/MoreOptions';
 import * as S from './NavNotes.styles';
+import { useNotesActions, useNotesState } from 'context/notesContext';
 
 const NavNotes = () => {
     const [showMoreOptions, setShowMoreOptions] = useState(false);
+    const { notes } = useNotesState();
+    const { updateManyNotes, removeManyNotes } = useNotesActions();
     const btnRef = useRef<HTMLButtonElement>(null!);
 
     const handleToggleMoreOptions = () => {
@@ -14,6 +17,39 @@ const NavNotes = () => {
 
     const handleCloseMoreOptions = () => {
         setShowMoreOptions(false);
+    }
+
+    const handleRemoveAllNotes = () => {
+        handleCloseMoreOptions();
+        removeManyNotes(notes.map(note => note.id));
+    }
+
+    const handlePriorityAllNotes = () => {
+        handleCloseMoreOptions();
+        updateManyNotes(notes.map(note => note.id), {
+            priority: true
+        });
+    }
+
+    const handleUnpriorityAllNotes = () => {
+        handleCloseMoreOptions();
+        updateManyNotes(notes.map(note => note.id), {
+            priority: false
+        });
+    }
+
+    const handleDoneAllNotes = () => {
+        handleCloseMoreOptions();
+        updateManyNotes(notes.map(note => note.id), {
+            done: new Date()
+        });
+    }
+
+    const handleUndoneAllNotes = () => {
+        handleCloseMoreOptions();
+        updateManyNotes(notes.map(note => note.id), {
+            done: null
+        });
     }
 
     return (
@@ -27,11 +63,11 @@ const NavNotes = () => {
                     onClick={handleToggleMoreOptions}
                 >b</Button>}
             >
-                <Button>usuń</Button>
-                <Button>ważne</Button>
-                <Button>nieważne</Button>
-                <Button>zrobione</Button>
-                <Button>niezrobione</Button>
+                <Button onClick={handleRemoveAllNotes}>usuń</Button>
+                <Button onClick={handlePriorityAllNotes}>ważne</Button>
+                <Button onClick={handleUnpriorityAllNotes}>nieważne</Button>
+                <Button onClick={handleDoneAllNotes}>zrobione</Button>
+                <Button onClick={handleUndoneAllNotes}>niezrobione</Button>
             </MoreOptions>
             <S.Link to='/all' replace>ogółem</S.Link>
             <S.Link to='/undone' replace>nie zrobione</S.Link>
