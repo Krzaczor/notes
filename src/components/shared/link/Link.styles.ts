1b6a09744a7955a33ce71a51660a328d417fa43c
Krzaczor <jbielecki18@gmail.com>
2022-06-05 21:10:39 +0200

    create link component

---
+++ src/components/shared/link/Link.styles.ts
@@ -0,0 +1,66 @@
+// import { NavLink } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { Props } from './Link.types';
+
+const variants = {
+    'primary': css`
+        background-color: var(--color-primary);
+        color: var(--color-light);
+    `,
+    'second': css`
+        background-color: var(--color-background);
+        color: var(--color-light);
+    `,
+    'outline-danger': css`
+        border-color: var(--color-danger);
+        color: var(--color-text);
+    `,
+    'dark': css`
+        background-color: var(--color-background-darkest);
+        color: var(--color-text);
+    `,
+}
+
+const sizes = {
+    'normal': css`
+        font-size: 1rem;
+        padding: 0.5rem;
+    `,
+    'lg': css`
+        font-size: 1.1rem;
+        padding: 0.7rem;
+    `
+}
+
+const fluids = css`
+    width: 100%;
+`;
+
+export const Link = styled(RouterLink)<Props>`
+    background-color: transparent;
+    border: 1px solid transparent;
+    border-radius: var(--radius);
+    color: var(--color-text);
+    cursor: pointer;
+    display: inline-block;
+    font-size: 1rem;
+    min-width: 40px;
+    padding: 0.5rem;
+    text-decoration: none;
+    text-align: center;
+    vertical-align: middle;
+
+    & svg {
+        vertical-align: middle;
+    }
+
+    &.active {
+        background-color: var(--color-background-darkest);
+        color: var(--color-text);
+    }
+
+    ${({ variant }) => variant && variant in variants ? variants[variant] : null};
+    ${({ size }) => size && size in sizes ? sizes[size] : null};
+    ${({ fluid }) => fluid ? fluids : null};
+`;
---
+++ src/components/shared/link/Link.types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+export interface Props {
+    children: ReactNode;
+    variant?: 'primary' | 'second' | 'outline-danger' | 'dark';
+    size?: 'normal' | 'lg';
+    fluid?: boolean;
+}
