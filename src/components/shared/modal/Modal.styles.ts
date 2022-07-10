d71c266b2dcd05b6b82a715b0d504fced4504096
Krzaczor <jbielecki18@gmail.com>
2022-07-10 14:10:12 +0200

    change width and order styles in modal

--- src/components/shared/modal/Modal.styles.ts
+++ src/components/shared/modal/Modal.styles.ts
@@ -1,33 +1,32 @@
 import styled from 'styled-components';
 
 export const ModalContainer = styled.div`
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
     align-items: center;
+    bottom: 0;
+    display: flex;
     justify-content: center;
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
     z-index: 20;
 `;
 
 export const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.6);
+    height: 100%;
     position: fixed;
     width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.6);
 `;
 
 export const Modal = styled.div`
     background-color: var(--color-background);
-    border-radius: 5px;
+    border-radius: var(--radius);
     color: var(--color-text);
-    min-width: 300px;
-    max-width: 60%;
     max-height: calc(100% - 40px);
     overflow-y: auto;
     padding: 20px;
+    width: min(80%, 25em);
     z-index: 21;
 
     & > *:not(:last-child) {
