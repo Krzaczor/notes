da68ea33d5778dde11364c0987b044283e6ab225
Krzaczor <jbielecki18@gmail.com>
2022-07-12 18:45:25 +0200

    change import format in main component

--- src/components/shared/main/Main.tsx
+++ src/components/shared/main/Main.tsx
@@ -1,6 +1,1 @@
-import * as S from './Main.styles';
-import { Props } from './Main.types';
-
-const Main = (props: Props) => <S.Main {...props} />;
-
-export default Main;
+export { Main as default } from './Main.styles';
