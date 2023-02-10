8d7868669903e33ccc04b17fe35c4f8577039894
Krzaczor <jbielecki18@gmail.com>
2023-02-10 02:34:29 +0100

    refactor useLightMode hook

--- src/hooks/useLightMode.ts
+++ src/hooks/useLightMode.ts
@@ -1,18 +1,18 @@
-import { useEffect, useRef } from 'react';
-import { useSettingsState } from 'context/settingsContext'
+import { useLayoutEffect, useRef } from 'react';
+import { useSettingsState } from 'context/settingsContext';
 
 export const useLightMode = () => {
     const firstRender = useRef(true);
     const lightMode = useSettingsState((s) => s.settings.lightMode);
 
-    document.querySelector('html')?.classList.toggle('light-mode', lightMode ?? false);
+    document.querySelector('html')?.classList.toggle('light-mode', lightMode);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (firstRender.current) {
             firstRender.current = false;
             return;
         }
 
-        document.querySelector('html')?.classList.toggle('light-mode', lightMode ?? false);
+        document.querySelector('html')?.classList.toggle('light-mode', lightMode);
     }, [lightMode]);
 }
