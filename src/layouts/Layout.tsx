fee77673564d81965244a02a6e6c18a0cff808b8
Krzaczor <jbielecki18@gmail.com>
2023-02-24 22:48:24 +0100

    remove unused modal info about error from localstorage

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,24 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import CommonPage from 'pages/CommonPage';
 import PanelCategories from 'components/panelCategories/PanelCategories';
-import Settings from 'components/errorStorage/settings/Settings';
 
 const Layout = () => {
     return (
-        <>
-            {null && <Settings />} 
-            <div style={{ display: 'flex', height: '100%' }}>
-                <PanelCategories>
-                    <PanelCategories.Title />
-                    <PanelCategories.Form />
-                    <PanelCategories.CategoriesList />
-                </PanelCategories>
-                <div style={{ flex: 1, overflow: 'hidden' }}>
-                    <CommonPage />
-                    <Outlet />
-                </div>
+        <div style={{ display: 'flex', height: '100%' }}>
+            <PanelCategories>
+                <PanelCategories.Title />
+                <PanelCategories.Form />
+                <PanelCategories.CategoriesList />
+            </PanelCategories>
+            <div style={{ flex: 1, overflow: 'hidden' }}>
+                <CommonPage />
+                <Outlet />
             </div>
-        </>
+        </div>
     )
 }
 
